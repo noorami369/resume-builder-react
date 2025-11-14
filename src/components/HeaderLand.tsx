d673519c7df433b7af58da7ef60522ec7145387e
noorami <noorami0369@gmail.com>
2025-11-14 10:46:00 +0330

    headerland-final-v

---
+++ src/components/HeaderLand.tsx
@@ -0,0 +1,209 @@
+import React from "react";
+import logo from "../image/logo.svg";
+import aiProfile1 from "../image/ai-profile-1.jpg";
+import aiProfile2 from "../image/ai-profile-2.jpg";
+import aiProfile3 from "../image/ai-profile-3.jpg";
+import aiProfile4 from "../image/ai-profile-4.jpg";
+import instagram from "../image/instagram.svg";
+import farmer from "../image/framer.svg";
+import microsoft from "../image/microsoft.svg";
+import huawei from "../image/huawei.svg";
+import walmart from "../image/walmart.svg";
+const HeaderLand = () => {
+  return (
+    <div className="min-h-screen pb-20">
+      <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
+        <img src={logo} alt="" />
+        <ul className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800 cursor-pointer">
+          <li className="hover:text-green-600 transition">Home</li>
+          <li className="hover:text-green-600 transition">Features</li>
+          <li className="hover:text-green-600 transition">Testimonials</li>
+          <li className="hover:text-green-600 transition">Contact</li>
+        </ul>
+        <div className="flex gap-2">
+          <button className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white cursor-pointer">
+            Get Started
+          </button>
+          <button className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900 cursor-pointer">
+            Login
+          </button>
+        </div>
+      </nav>
+      <div className="relative flex flex-col items-center justify-center text-sm px-4 md:px-16 lg:px-24 xl:px-40 text-black">
+        <div className="absolute top-28 xl:top-10 -z-10 left-1/4 size-72 sm:size-96 xl:size-120 2xl:size-132 bg-green-300 blur-[100px] opacity-30"></div>
+        <div className="flex items-center mt-24">
+          <div className="flex -space-x-3 pr-3">
+            <img
+              className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-1"
+              src={aiProfile1}
+              alt=""
+            />
+            <img
+              className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-2"
+              src={aiProfile2}
+              alt=""
+            />
+            <img
+              className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-3"
+              src={aiProfile3}
+              alt=""
+            />
+            <img
+              className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[4]"
+              src={aiProfile1}
+              alt=""
+            />
+            <img
+              className="size-8 rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[5]"
+              src={aiProfile4}
+              alt=""
+            />
+          </div>
+          <div>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+            </div>
+            <p className="text-sm text-gray-700">Used by 10,000+ users</p>
+          </div>
+        </div>
+        <h1 className="text-5xl md:text-6xl font-semibold max-w-5xl text-center mt-4 md:leading-[70px]">
+          Land your dream job with
+          <span className=" bg-linear-to-r from-green-700 to-green-600 bg-clip-text text-transparent text-nowrap">
+            AI-powered
+          </span>
+          resumes.
+        </h1>
+        <p className="max-w-md text-center text-base my-7">
+          Create, edit and download professional resumes with AI-powered
+          assistance.
+        </p>
+        <div className="flex items-center gap-4 ">
+          <button className="bg-green-500 hover:bg-green-600 text-white rounded-full px-9 h-12 m-1 ring-offset-2 ring-1 ring-green-400 flex items-center transition-colors cursor-pointer">
+            Get started
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-arrow-right ml-1 size-4"
+              aria-hidden="true"
+            >
+              <path d="M5 12h14"></path>
+              <path d="m12 5 7 7-7 7"></path>
+            </svg>
+          </button>
+          <button className="flex items-center gap-2 border border-slate-400 hover:bg-green-50 transition rounded-full px-7 h-12 text-slate-700 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-video size-5"
+              aria-hidden="true"
+            >
+              <path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path>
+              <rect x="2" y="6" width="14" height="12" rx="2"></rect>
+            </svg>
+            <span>Try demo</span>
+          </button>
+        </div>
+        <p className="py-6 text-slate-600 mt-14">
+          Trusting by leading brands, including
+        </p>
+        <div className="flex flex-wrap justify-between max-sm:justify-center gap-6 max-w-3xl w-full mx-auto py-4">
+          <img className="h-6 w-auto max-w-xs" src={instagram} alt="" />
+          <img className="h-6 w-auto max-w-xs" src={farmer} alt="" />
+          <img className="h-6 w-auto max-w-xs" src={microsoft} alt="" />
+          <img className="h-6 w-auto max-w-xs" src={huawei} alt="" />
+          <img className="h-6 w-auto max-w-xs" src={walmart} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderLand;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AiAlert from "../components/AiAlert";
+import HeaderLand from "../components/HeaderLand";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col">
+      <AiAlert />
+      <HeaderLand />
+    </div>
+  );
+};
+
+export default Home;
